753461ada2b5a0aa651f7eb13d1def1ea89e6938
hanifadzkiya <hanif@oyindonesia.com>
2020-09-19 14:34:41 +0700

    improvement relu

--- src/layers/activation.py
+++ src/layers/activation.py
@@ -6,10 +6,4 @@ from src.layers.base_layer import Layer
 
 class Activation(Layer):
     def forward(self, input):
-        result = copy.deepcopy(input)
-
-        for i in range(np.size(result, 0)):
-            for j in range(np.size(result, 1)):
-                result[i][j] = max(0, result[i][j])
-
-        return copy.deepcopy(result)
+        return np.maximum(input, 0)
